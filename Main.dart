1f2c2dcbfc2e2aeb5b6c5c09768c0c2fea2e0262
leandroguevara67 <108943012+leandroguevara67@users.noreply.github.com>
2022-12-13 10:18:03 -0300

    Create Main.dart

---
+++ Main.dart
@@ -0,0 +1,22 @@
+//Siempre consultar todo el codigo para apender en https://esflutter.dev/
+import 'package:flutter/material.dart'; //Con esto importamos el packete para trabajar con Dart y Flutter
+
+void main(){
+ runApp(MaterialApp(
+ home: Scaffold(
+  appBar: AppBar(
+   title: Text('Mejor que Naturgy'),   
+                    ),
+       body:Center(
+         child:Text('Hola Mundo', style: TextStyle(
+          color: Colors.deepOrangeAccent,
+          fontSize: 70.5,
+          fontWeight: FontWeight.w900                                 
+                                                  ), 
+                    ),        
+                   ),        
+                ),     
+                    ),
+       );
+
+           }
